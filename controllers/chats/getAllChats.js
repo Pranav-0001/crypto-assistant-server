c0248469ddda5ca16e4c46a2bcbe15671f9c765d
Pranav C <122021532+Pranav-0001@users.noreply.github.com>
2025-06-16 14:39:03 +0530

    fix

--- controllers/chats/getAllChats.js
+++ controllers/chats/getAllChats.js
@@ -2,6 +2,7 @@ import { matchedData } from "express-validator";
 import chatModel from "../../model/chatModel.js";
 import paginatior from "../../utils/paginator.js";
 import { response200 } from "../../utils/successResponses.js";
+import { internalServerError } from "../../utils/errorResponses.js";
 
 export default async function getAllChats(req, res) {
   try {
